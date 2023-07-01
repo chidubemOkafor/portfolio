70c8e86f7612f3e99cd21f78f3849fe23684a091
Robinhood <okaforchidubem7@gmail.com>
2023-07-01 20:53:55 +0100

    addedlinks

--- components/main/Main.jsx
+++ components/main/Main.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
-import {AiOutlineTwitter} from "react-icons/ai"
+import {AiOutlineTwitter,AiFillGithub} from "react-icons/ai"
 import {BsLinkedin,BsDiscord} from "react-icons/bs"
-import {Wallet} from "../useConnectWallet.js"
+import {connectWallet} from "../useConnectWallet.js"
 import css from "../../src/assets/icons/css.png"
 import react from "../../src/assets/icons/react.gif"
 import python from "../../src/assets/icons/python.png"
@@ -12,8 +12,9 @@ import javascriot from "../../src/assets/icons/javascript.png"
 import ReactAudioPlayer from "react-audio-player"
 import './Main.css'
 
+
 function Main(prop) {
-  const wallet = new Wallet()
+  // const wallet = new Wallet()
   const [connected, setConnected] = useState(false)
 
   const text_buttons = {
@@ -41,14 +42,15 @@ function Main(prop) {
               </div>
                 
                 <div className='icons' style={text_buttons}>
-                  <AiOutlineTwitter className='twitter'/>
-                  <BsLinkedin className='linkedin'/>
-                  <BsDiscord className='discord'/>
+                  <a href='https://twitter.com/Okaforchidex7'><AiOutlineTwitter className='twitter'/></a>
+                  <a href='https://www.linkedin.com/in/chidubem-okafor-51648922b'><BsLinkedin className='linkedin'/></a>
+                  <a href='https://discord.gg/9sMNeDZ7'><BsDiscord className='discord'/></a>
+                  <a href='https://github.com/chidubemOkafor'><AiFillGithub className='github'/></a>
                 </div>
                 <div className='stack'>
                   <p>Tech Stack  |</p>
                     <div className='individual_stack'>
-                    <img src={html}/>
+                    <img src={html} />
                     <img src={css}/>
                     <img src={javascriot}/>
                     <img src={react}/>
@@ -61,7 +63,7 @@ function Main(prop) {
                 <div className='button_div'>
                   <button>download cv</button>
                   <button>hire me</button>
-                  {!connected ? <button onClick={() =>  wallet.connect()}>connect wallet</button>: <button>mint cv</button>}
+                  {!connected ? <button onClick={connectWallet}>connect wallet</button>: <button>mint cv</button>}
                 </div>
             </div>
             <audio
--- components/useConnectWallet.js
+++ components/useConnectWallet.js
@@ -1,41 +1,31 @@
-import { ethers } from "ethers"
+import { ethers } from "ethers";
 
-// export const connectWallet = async() => {
-//   try {
-//      if(window.ethereum != undefined) {
-//         const provider = new ethers.providers.Web3Provider(window.ethereum)
-//         await provider.send("eth_requestAccounts", []);
-//         const signer = provider.getSigner()
-//         return signer
-        
-//      } else {
-//         alert("you need to install metamask https://metamask.io/")
-//      }
-//   }catch(err) {
-//     console.error(err)
-//   }
-// }
+export const connectWallet = async() => {
+  try {
+     if(window.ethereum != undefined) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      // MetaMask requires requesting permission to connect users accounts
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner()
+      return signer;
+     } else {
+        alert("you need to install metamask https://metamask.io/")
+     }
+  }catch(err) {
+    console.error(err)
+  }
+}
 
 export class Wallet {
-   
     async connect() {
         try {
             if(window.ethereum != undefined) {
-                const provider = new ethers.providers.Web3Provider(window.ethereum)
-                await provider.send("eth_requestAccounts", []);
-                const signer = provider.getSigner()
-                this.signer = signer
-                
-            } else {
-                alert("you need to install metamask https://metamask.io/")
+                  const provider = new ethers.providers.Web3Provider(window.ethereum)
+                  await provider.send("eth_requestAccounts", []);
+                  const signer = provider.getSigner()
             }
         } catch(err) {
             console.error(err)
         }
-
-    }
-
-    getSigner () {
-        return this.signer 
     }
 }
