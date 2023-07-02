8d312c84f171dcc5c5894c7121cea1f511da91a5
Robinhood <okaforchidubem7@gmail.com>
2023-07-02 22:04:48 +0100

    added some features in the navbar

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {GoHome} from "react-icons/go"
 import {AiOutlineInfoCircle} from "react-icons/ai"
-import {GoProjectRoadmap} from "react-icons/go"
+import {MdWorkOutline} from "react-icons/md"
 import {RiContactsLine} from "react-icons/ri"
 import "./Navbar.css"
 
@@ -12,7 +12,7 @@ const Navbar = () => {
             <div className='navbar_container'>
                 <GoHome className='navbar_icon'/>
                 <AiOutlineInfoCircle className='navbar_icon'/>
-                <GoProjectRoadmap className='navbar_icon'/>
+                <MdWorkOutline className='navbar_icon'/>
                 <RiContactsLine className='navbar_icon'/>
             </div>
         </div>
