32e81f395293f22dc0e2015283cc87f0cba47739
Robinhood <okaforchidubem7@gmail.com>
2023-07-11 22:33:20 +0100

    responsiveness 2

--- client/components/navbar/Navbar.jsx
+++ client/components/navbar/Navbar.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {GoHome} from "react-icons/go"
 import {AiOutlineInfoCircle} from "react-icons/ai"
 import {MdWorkOutline} from "react-icons/md"
 import {RiContactsLine} from "react-icons/ri"
+import {FiX} from "react-icons/fi"
 import {GiHamburgerMenu} from "react-icons/gi"
 import "./Navbar.css"
 
 const Navbar = (prop) => {
+  const [isNavBar, setIsNavBar] = useState(false)
+  const toggleBar = () => setIsNavBar(!isNavBar)
   return (
     <div>
         <div className='second_navbar_container'>
           <div className='gap_of'>
             <p className='my_name'>Okafor Chidubem</p>
-            <GiHamburgerMenu className='hamburger'/>
+            {! isNavBar ? <GiHamburgerMenu onClick={toggleBar} className={`hamburger ${!isNavBar ? 'rotate' : ''}`}/> : <FiX onClick={toggleBar} className='hamburger'/>}
           </div>
         </div>
-        <div className='roll_down'>
+        {isNavBar ?<div className='roll_down'>
           <p className='bar'>Home</p>
           <p className='bar'>About</p>
           <p className='bar'>Portfolio</p>
           <p className='bar'>Contact</p>
-        </div>
+        </div> : <></>}
     </div>
   )
 }
